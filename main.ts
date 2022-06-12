d0769d9b4956ba6ad7b1552f835d42b8d74a5a0d
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-12 16:40:29 +0200

    first prototype

--- main.ts
+++ main.ts
@@ -1,9 +1,41 @@
-import { Plugin } from "obsidian";
+import { MarkdownView, Plugin, debounce } from "obsidian";
 
 export default class TaskStatusBar extends Plugin {
+	taskInStatusBar: HTMLElement;
 
-	async onload() { console.log("Task Status Bar Plugin loaded.") }
+	async onload() {
+		console.log("Task Status Bar Plugin loaded.");
+		const countDelay = 1000;
+
+		this.taskInStatusBar = this.addStatusBarItem();
+		this.taskInStatusBar.setText("");
+
+		this.registerEvent(
+			this.app.workspace.on("file-open", this.countTasks)
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("editor-change", debounce(this.countTasks, countDelay, true))
+		);
+
+	}
 
 	async onunload() { console.log("Task Status Bar Plugin unloaded.") }
 
+
+	countTasks = () => {
+		const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+		let newText = "";
+		if (mdView !== null && mdView.getViewData()) {
+			const content = mdView.getViewData();
+			const openTasks = content.match(/^\s*- \[ ] /g).length;
+			const completedTasks = content.match(/^\s*- \[x] /g).length;
+
+			const overallTasks = completedTasks + openTasks;
+
+			if (overallTasks) newText = completedTasks + "/"+ overallTasks + " tasks";
+		}
+		this.taskInStatusBar.setText(newText);
+	};
 }
